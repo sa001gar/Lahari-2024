4b0458710d3e66b9b89111bcf4a10bb7b4bda7f6
sa001gar <kundusagar233@gmail.com>
2024-12-22 01:08:07 +0530

    Updating The site after Lahari

--- details.js
+++ details.js
@@ -1,43 +1,44 @@
+// Place Holder Link --> https://via.placeholder.com/50?text=RC
 const projects = [
     {
         title: "AI Powered Electricity Theft Detection & Maneuvering System",
-        image: "images/project_images/project_4.jpeg",
+        image: "images/project_images/electricity_theft.jpeg",
         description: "The AI-Powered Electricity Theft Detection & Maneuvering System is an innovative solution to combat electricity theft using AI and IoT technologies. It monitors energy consumption, detects anomalies, and provides real-time alerts via a centralized dashboard. The system triggers automated responses like buzzers and grid isolation to prevent losses. It enhances power distribution efficiency, reduces energy theft, and ensures a secure, reliable grid.",
         members: [
             { name: "Sagar Kundu", image: "images/profile_image/sagar_kundu.jpeg", contribution: "Project Manager", social: { linkedin: "https://www.linkedin.com/in/sagarkundu/", github: "https://github.com/sa001gar", twitter: "#" } },
             { name: "Koyel Das", image: "images/profile_image/koyel_das.jpeg", contribution: "Team Member", social: { linkedin: "", github: "https://github.com/koyel001", twitter: "#" } },
-            { name: "Akash Karmakar", image: "https://via.placeholder.com/50?text=AK", contribution: "Team Lead", social: { linkedin: "https://www.linkedin.com/in/akash-karmakar-912a48315", github: "https://github.com/akashkarmakar22", twitter: "akashkarmakar2323@gmail.com" } },
+            // { name: "Akash Karmakar", image: "https://via.placeholder.com/50?text=AK", contribution: "Team Lead", social: { linkedin: "https://www.linkedin.com/in/akash-karmakar-912a48315", github: "https://github.com/akashkarmakar22", twitter: "mailto:akashkarmakar2323@gmail.com" } },
             { name: "Akash Paramanik", image: "images/profile_image/akash_paramanik.jpeg", contribution: "Team Member", social: { linkedin: "https://www.linkedin.com/in/akash-paramanik-b28307316/", github: "https://github.com/Akash2004Paramanik", twitter: "mailto:paramanikakash04@gmail.com" } },
-            { name: "Debarghya Dutta", image: "https://via.placeholder.com/50?text=DD", contribution: "Team Member", social: { linkedin: "", github: "#", twitter: "#" } },
-            { name: "Souradeep Dhara", image: "https://via.placeholder.com/50?text=SD", contribution: "Team Member", social: { linkedin: "", github: "#", twitter: "#" } },
+            { name: "Debarghya Dutta", image: "images/profile_image/debarghya_dutta.jpeg", contribution: "Team Member", social: { linkedin: "https://www.linkedin.com/in/debarghya-dutta-596a57315", github: "https://github.com/DEBARGHYADUTTA2004", twitter: "mailto:duttadebarghya277@gmail.com" } },
+            // { name: "Souradeep Dhara", image: "https://via.placeholder.com/50?text=SD", contribution: "Team Member", social: { linkedin: "", github: "#", twitter: "#" } },
             
         ]
     },
     {
         title: "Fire Fighter",
-        image: "images/project_images/fire.jpeg",
+        image: "images/project_images/fire_fighter.jpeg",
         description: "The Firefighter Robot using Arduino Uno is an autonomous robotic system designed to detect and extinguish fires. It uses flame sensors for fire detection, ultrasonic sensors for navigation, and a water pump or fan for extinguishing flames. Controlled by Arduino, the robot operates independently in hazardous areas, ensuring safety. This project highlights the integration of robotics and automation in fire safety management",
         members: [
             { name: "Chaiti Laha", image: "images/profile_image/chaiti_laha.jpeg", contribution: "Team Lead", social: { linkedin: "https://www.linkedin.com/in/chaiti-laha-473abb315", github: ":https://github.com/chaiti7029", twitter: "mailto:chaitilaha5@gmail.com" } },
             { name: "Tuhina Pal", image: "images/profile_image/tuhina_pal.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
             { name: "Suchana Mondal", image: "images/profile_image/suchana_mondal.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
             { name: "Rajdip Mondal", image: "images/profile_image/rajdip_mondal.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "mailto:mondalrajdip777@gmail.com" } },
-            { name: "Ratul Chatterjee", image: "https://via.placeholder.com/50?text=RC", contribution: "Project Manager", social: { linkedin: "#", github: "#", twitter: "#" } },
+            { name: "Ratul Chatterjee", image: "images/profile_image/ratul_chatterjee.jpeg", contribution: "Project Manager", social: { linkedin: "#", github: "#", twitter: "#" } },
             { name: "Ankita Mondal", image: "images/profile_image/ankita_mondal.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "mailto:missmondal10@gmail.com" } },
         ]
     },
     {
         title: "Plant Irrigation System",
-        image: "images/project_images/plant_irrigation_system.jpeg",
+        image: "images/project_images/plant_irrigation.jpeg",
         description: "A plant irrigation system delivers water to plants efficiently, ensuring healthy growth and minimizing waste. It can include manual methods, sprinklers, drip systems, or soaker hoses. Advanced systems use sensors to monitor soil moisture and automate watering as needed. These systems are crucial in agriculture, gardening, and landscaping. They save water, reduce labor, and boost plant productivity.",
         members: [
-            { name: "Irsha Sarkar ", image: "https://via.placeholder.com/50?text= IS", contribution: "Team Lead", social: { linkedin: "#", github: "#", twitter: "mailto:Irshasarkar2004@gmail.com" } },
-            { name: "Aparajita Kalsi", image: "https://via.placeholder.com/50?text=AK", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
-            { name: "Aparajita Mukherjee ", image: "https://via.placeholder.com/50?text=AM", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
+            { name: "Irsha Sarkar ", image: "images/profile_image/irsha_sarkar.jpeg", contribution: "Team Lead", social: { linkedin: "#", github: "#", twitter: "mailto:Irshasarkar2004@gmail.com" } },
+            { name: "Aparajita Kalsi", image: "images/profile_image/aparajita_kalsi.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
+            { name: "Aparajita Mukherjee ", image: "images/profile_image/aparajita_mukherjee.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "aparajita24122004@gmail.com" } },
             { name: "Sruti Mallick", image: "images/profile_image/sruti_mallick.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
-            { name: "Isha Das", image: "https://via.placeholder.com/50?text= ID", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
-            { name: "Rajita Chakraborty ", image: "https://via.placeholder.com/50?text=RC", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
-            { name: "Sangeeta Banerjee ", image: "https://via.placeholder.com/50?text=SB", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } }
+            { name: "Isha Das", image: "images/profile_image/isha_das.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
+            { name: "Rajita Chakraborty ", image: "images/profile_image/rajita_chakraborty.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
+            { name: "Sangita Banerjee  ", image: "images/profile_image/sangita_banerjee.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } }
         ]
     },
     {
@@ -46,49 +47,49 @@ const projects = [
         description: "A Pollution Mitigation Project aims to reduce environmental contaminants through various strategies and technologies. It focuses on minimizing air, water, and soil pollution by implementing sustainable practices. The project involve  renewable energy sources and promoting eco-friendly behaviors. It also emphasizes public awareness and education on pollution's impacts. Ultimately, the aim is to create cleaner, healthier environments and mitigate the effects of pollution for future generations.",
         members: [
             { name: "Swarnamoyee Sikder", image: "images/profile_image/swarnamoyee_sikder.jpeg", contribution: "Team Lead", social: { linkedin: "https://www.linkedin.com/in/swarnamoyee-sikder-495574333", github: "#", twitter: "mailto:swarnaskdr02@gmail.com" } },
-            { name: "Samapti Ghosh", image: "https://via.placeholder.com/50?text=SG", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
+            { name: "Samapti Ghosh", image: "images/profile_image/samapti_ghosh.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "mailto: samaptighosh678@gmail.com" } },
             { name: "Disha Hazra", image: "images/profile_image/disha_hazra.jpeg", contribution: "Team Member", social: { linkedin: "https://www.linkedin.com/in/disha-hazra-b74172342", github: "#", twitter: "mailto:dishahazra2004@gmail.com" } },
         ]
     },
     {
         title: "DriveSafe : Your personal safety companion",
-        image: "images/profile_image/drive_safe.jpeg",
+        image: "images/project_images/drive_safe.jpeg",
         description: "This app defines a safety app that uses the device's accelerometer to detect shakes and trigger emergency actions. It allows users to manage emergency contacts, set shake sensitivity, and send location-based alerts through email. The app features a countdown timer for emergency situations, speech feedback, along with options to cancel or adjust the emergency settings. The UI includes a modal for adding/removing contacts and a slider to adjust shake sensitivity.",
         members: [
             { name: "Debjit Goswami", image: "images/profile_image/debjit_goswami.jpeg", contribution: "Team Lead", social: { linkedin: "https://www.linkedin.com/in/debjit-goswami-88303930a/", github: "https://github.com/debjitgoswami", twitter: "mailto:debjitg421@gmail.com" } },
             { name: "Jotirmoyee Ghosh", image: "images/profile_image/jyotirmoyee.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "mailto:ghoshjoti655@gmail.com" } },
             { name: "Jannhabi Dhar", image: "images/profile_image/janhabi.jpeg", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "mailto:enma210622@gmail.com" } },
-            { name: "Amon Dey", image: "https://via.placeholder.com/50?text=AD", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
+            // { name: "Amon Dey", image: "https://via.placeholder.com/50?text=AD", contribution: "Team Member", social: { linkedin: "#", github: "#", twitter: "#" } },
         
         ]
     },
     {
         title: "Anti sleep alarm system",
-        image: "images/profile_image/anti.webp",
+        image: "images/project_images/anti_sleep.jpeg",
         description: "Sleep-related accidents, particularly those caused by drowsy driving or fatigue in workplaces, have become a growing concern worldwide. The Anti-Sleep Alarm System is an innovative technology designed to address this critical issue by preventing incidents caused by sleep-induced lapses in attention.This system functions as a proactive safety device, typically worn or installed in vehicles, to monitor the user\'s alertness. By detecting signs of drowsiness or inattention, it alerts the individual through auditory, visual, or vibratory signals, helping them regain focus and avoid potential accidents.",
         members: [
-            { name: "Anirban Chakraborty", image: "https://via.placeholder.com/50?text=JD", contribution: "Team Lead", social: { linkedin: "https://www.linkedin.com/in/anirban-chakraborty-442291310", github: "#", twitter: "#" } },
+            { name: "Anirban Chakraborty", image: "https://via.placeholder.com/50?text=AC", contribution: "Team Lead", social: { linkedin: "https://www.linkedin.com/in/anirban-chakraborty-442291310", github: "#", twitter: "#" } },
             
         ]
     },
     {
         title: "Revival of Energy",
-        image: "images/profile_image/revival.webp",
-        description: "Sleep-related accidents, particularly those caused by drowsy driving or fatigue in workplaces, have become a growing concern worldwide. The Anti-Sleep Alarm System is an innovative technology designed to address this critical issue by preventing incidents caused by sleep-induced lapses in attention.This system functions as a proactive safety device, typically worn or installed in vehicles, to monitor the user\'s alertness. By detecting signs of drowsiness or inattention, it alerts the individual through auditory, visual, or vibratory signals, helping them regain focus and avoid potential accidents.",
+        image: "images/project_images/revival.webp",
+        description: "Revival of Energy is a step towards a sustainable future, blending technology with nature. It focuses on harnessing renewable energy sources like solar and wind to reduce dependence on fossil fuels. By promoting clean energy solutions, it aims to combat pollution and environmental degradation. Together, we can power a greener, brighter tomorrow!",
         members: [
-            { name: "Narayan Mondal", image: "images/profile_image/narayan.jpeg", contribution: "Team Lead", social: { linkedin: "https://www.linkedin.com/in/anirban-chakraborty-442291310", github: "#", twitter: "#" } },
+            { name: "Narayan Mondal", image: "images/profile_image/narayan_mondal.jpg", contribution: "Team Lead", social: { linkedin: "https://www.linkedin.com/in/anirban-chakraborty-442291310", github: "https://github.com/mondalll", twitter: "https://x.com/narayanmondall?s=08" } },
             
         ]
     },
-    {
-        title: "FollowUP : a line mangement web app",
-        image: "images/project_images/line.png",
-        description: "Streamline your waiting experience with our line management app! Users can easily register for queues, track their position in real-time, and view estimated wait times. Organizations can efficiently create and manage queues, schedules, and capacity. Enjoy a hassle-free, organized system that saves time and enhances convenience!",
-        members: [
-            { name: "Narayan Mondal", image: "https://via.placeholder.com/50?text=NM", contribution: "Team Lead", social: { linkedin: "https://www.linkedin.com/in/anirban-chakraborty-442291310", github: "#", twitter: "#" } },
+    // {
+    //     title: "FollowUP : a line mangement web app",
+    //     image: "images/project_images/line.png",
+    //     description: "Streamline your waiting experience with our line management app! Users can easily register for queues, track their position in real-time, and view estimated wait times. Organizations can efficiently create and manage queues, schedules, and capacity. Enjoy a hassle-free, organized system that saves time and enhances convenience!",
+    //     members: [
+    //         { name: "Narayan Mondal", image: "https://via.placeholder.com/50?text=NM", contribution: "Team Lead", social: { linkedin: "https://www.linkedin.com/in/anirban-chakraborty-442291310", github: "#", twitter: "#" } },
             
-        ]
-    }
+    //     ]
+    // }
 ];
 
 function createProjectElement(project) {
